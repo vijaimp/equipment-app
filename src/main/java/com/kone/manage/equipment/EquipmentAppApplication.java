bdf415c2d9c50673bec284c40be1e79cac2358a1
Vijai Panneer <vijai.panneer@efecte.com>
2021-07-10 11:47:57 +0300

    initial commit

---
+++ src/main/java/com/kone/manage/equipment/EquipmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.kone.manage.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EquipmentAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kone/manage/equipment/EquipmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kone.manage.equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
